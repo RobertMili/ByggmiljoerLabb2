8e38a13fc06d3c30522b8fa9a133f3021cf976ab
RobertMili <98566676+RobertMili@users.noreply.github.com>
2023-01-27 22:17:28 +0100

    with List and stream did get a bug

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -1,10 +1,49 @@
 package com.example.calculator;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.*;
+
 public class Calculator {
 
-    public static int Add(String numbers){
+    public static void main(String[] args) {
+        Add("\\n1,2");
+    }
 
-        int numInt = Integer.parseInt(numbers);
-        return numInt;
+    public static int Add(String numbers) {
+        String[] test = numbers.split(";\\n");
+        List<String> numberList = new ArrayList<>();
+        String test1 = "";
+        String resultString = "";
+
+
+       String[] stringNumbers =
+               numbers.split("//|/|,|;|\\n|\\*|%|]|\\[|\\\\|[a-z]");
+        numberList.add(numbers);
+
+
+        if (numbers.equals("")) {
+            return 0;
+        }
+
+
+        List<String> list = numberList.stream()
+                .flatMap(s -> Arrays.stream(s.split("//")))
+                .flatMap(s -> Arrays.stream(s.split("/")))
+                .flatMap(s -> Arrays.stream(s.split(",")))
+                .flatMap(s -> Arrays.stream(s.split(";")))
+                .flatMap(s -> Arrays.stream(s.split("\\*")))
+                .flatMap(s -> Arrays.stream(s.split("[a-z]")))
+                .toList();
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("Resultat " +list.get(i));
+            resultString += Integer.parseInt(list.get(i));
+
+        }
+
+        return result;
+
+
     }
 }
--- src/test/java/com/example/calculator/CalculatorTest.java
+++ src/test/java/com/example/calculator/CalculatorTest.java
@@ -1,28 +1,53 @@
 package com.example.calculator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CalculatorTest {
 
+    Calculator calculator = new Calculator();
+
+
     @Test
     void addOne() {
-        String number = "1";
+        var result =calculator.Add("1");
 
-        assertThat(number).isEqualTo("1");
+        assertThat(result).isEqualTo(1);
     }
     @Test
-    void addNothing() {
-        String number = "";
+    void addEmptyString() {
+        var result = calculator.Add("");
 
-        assertThat(number).isEqualTo("");
+        assertThat(result).isEqualTo(0);
     }
     @Test
     void addOneCommaTwo() {
-        String number = "1,2";
+        var result = calculator.Add("1,2");
 
-        assertThat(number).isEqualTo("1,2");
+        assertThat(result).isEqualTo(3);
     }
+
+    @Test
+    void addNumbersWithN(){
+        var result = calculator.Add("1\n2,3");
+
+        assertThat(result).isEqualTo(6);
+    }
+    @Test
+    void addNewLineAfterComma(){
+        var result = calculator.Add("1,\n");
+
+        assertThat(result).isEqualTo(1);
+    }
+    @Test
+    void addNewLineAndSemicolon(){
+        var result = calculator.Add("-1");
+
+        assertThat(result).isEqualTo(3);
+    }
+
 }
